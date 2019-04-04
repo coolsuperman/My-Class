70b97dbb87c11439d8c9bc6825b6a740e69ed558
coolsuperman <846069147@qq.com>
2019-04-04 21:36:41 +0800

    增加模板特性

--- SignleList/SList.h
+++ SignleList/SList.h
@@ -1,40 +1,263 @@
 #include<iostream>
 using namespace std;
-
+template <class T>
 struct ListNode{
-  int val;
-  ListNode* next;
-  void* operator new(size_t n);//重载自定义类型 new
-  void operator delete(void* p);//重载自定义类型 delete
+  T val;
+  ListNode<T> * next;
+  void* operator new(size_t n){//重载自定义类型 new
+    void*p = NULL;
+    p = allocator<ListNode>().allocate(1);
+    cout<<"allocate！"<<endl;
+    return p;
+  }
+
+//  istream& opetator>>(istream&ins,ListNode<T> target){
+//    ins>>
+//  }
+
+  void operator delete(void* p){//重载自定义类型 delete
+    allocator<ListNode>().deallocate((ListNode<T>*)p,1);
+    cout<<"deallocate！"<<endl;
+  }
+  
 };
 
+template <class T>
 class SList{
-  friend class Solution;
-  public:
-  SList(int* data_arr=NULL,int arrSize=0 )
+  typedef ListNode<T> Node;
+friend class Solution;
+private:
+  T* _data;
+  int _ListSize;
+  Node* _cur = NULL;
+  Node* _head = NULL;
+public:
+  SList(T* data_arr=NULL,int arrSize=0 )
     :_data(data_arr)
      ,_ListSize(arrSize)
   {}
-  SList(const SList& target);
-  ~SList();
-  ListNode*CreatAList();
-  ListNode*CreatAList_Loop(int num);
-  ListNode*SetNodeAfter(int nums,int val);  
-  ListNode*SetNodeBerfore(int nums,int val);
-  ListNode*SetNodeTail(int val);
-  ListNode*SetNodeHead(int val);
-  ListNode*DelNodeHead();
-  ListNode*DelNodeTail();
-  ListNode*DelNodeInsert(int pos);
-  ListNode*TakeNode(int num);
-  ListNode*TakeHalf(ListNode*start,ListNode*end);
-  void CheckNode_ByAddress(ListNode* address);
-  void CheckNode_Bynum(int num);
-  void DisplayList();
-  SList& operator=(const SList& d);
-  private:
-  int* _data;
-  int _ListSize;
-  ListNode* _cur = NULL;
-  ListNode* _head = NULL;
+  ~SList(){
+    while(_head!=NULL){
+      Node*self = _head;
+      _head = _head->next;
+      delete self;
+    }
+  }
+
+  SList(const SList& target){
+    this->_data = target._data;
+    this->_cur = target._cur;
+    this->_head = target._head;
+    this->_ListSize = target._ListSize;
+  }
+  int ListSize(){
+    return _ListSize;
+  }
+  SList& operator=(const SList& target){
+    if(&target!=this){
+      this->_data = target._data;
+      this->_cur = target._cur;
+      this->_head = target._head;
+      this->_ListSize = target._ListSize;
+    }
+    return *this;
+  }
+
+  Node* CreatAList(){
+    for(int i=0;i<_ListSize;i++){
+      if(i==0){
+        _cur = new Node;
+        _cur->val = *(_data+i);
+        _head = _cur;
+
+      }else{
+        Node* fresh = new Node;
+        fresh->val = *(_data+i);
+        _cur->next = fresh;
+        _cur = _cur->next;
+
+      }
+
+    }
+    _cur->next = NULL;
+    return _head;
+  }
+
+  Node* CreatAList_Loop(int pos){
+    for(int i=0;i<_ListSize;i++){
+      if(i==0){
+        _cur = new Node;
+        _cur->val = *(_data+i);
+        _head = _cur;
+
+      }else{
+        Node* fresh = new Node;
+        fresh->val = *(_data+i);
+        _cur->next = fresh;
+        _cur = _cur->next;
+
+      }
+    }
+    Node* loop_in = this->TakeNode(pos);
+    _cur->next = loop_in;
+    return _head;
+  }
+
+  Node* SetNodeHead(T val){
+    Node* fresh = new Node;
+    fresh->val = val;
+    fresh->next = _head;
+    _head = fresh;
+    _ListSize++;
+    return _head;
+  }
+
+  Node* SetNodeTail(T val){
+    if(_ListSize==0){
+      return this->SetNodeHead(val);
+    }
+    Node* fresh = new Node;
+    fresh->val=val;
+    Node* go = TakeNode(_ListSize-1);
+    fresh->next = NULL;
+    go->next = fresh;
+    _ListSize++;
+    return _head;
+  }
+
+  Node* SetNodeAfter(int pos,T val){
+    Node* fresh = new Node;
+    fresh->val = val;
+    if(pos<0||pos>_ListSize){
+      cout<<"出入位置错误"<<"_ListSize ="<<_ListSize<<endl;
+      delete fresh;
+    }
+    else if(_ListSize==0){
+      return this->SetNodeHead(val);
+    }
+    else if(pos==_ListSize){
+      return this->SetNodeTail(val);
+    }
+    else{
+      Node* go = TakeNode(pos);
+      fresh->next= go->next;
+      go->next = fresh;
+      _ListSize++;
+    }
+    return _head;
+  }
+
+  Node* SetNodeBerfore(int pos,T val){
+    Node*fresh = new Node;
+    fresh->val = val;
+    if(pos<0||pos>_ListSize){
+      cout<<"0~ListSize-1!"<<"_ListSize ="<<_ListSize<<endl;
+      delete fresh;
+    }
+    else if(_ListSize==0||pos==0){
+      return this->SetNodeHead(val);
+    }else if(pos==_ListSize){
+      return this->SetNodeTail(val);
+    }
+    else{
+      Node* go =TakeNode(pos-1);
+      fresh->next= go->next;
+      go->next = fresh;
+      _ListSize++;
+    }
+    return _head;
+  }
+
+  Node* DelNodeHead(){
+    if(!_head){return NULL;}
+    Node*get = _head;
+    _head = _head->next;
+    delete get;
+    _ListSize--;
+    return _head;
+  }
+
+  Node* DelNodeTail(){
+    if(!_head){return NULL;}
+    if(_ListSize==1){return DelNodeHead();}
+    Node*get = TakeNode(_ListSize-2),*last = get->next;
+    get->next =NULL;
+    _ListSize--;
+    delete last;
+    return _head;
+  }
+
+  Node* DelNodeInsert(int pos){
+    if((!_head)||pos==0){return DelNodeHead();}
+    if(pos<0||pos>_ListSize-1){
+      cout<<"input inleagel"<<endl;
+      return NULL;
+    }
+    Node*get = TakeNode(pos-1),*last = get->next;
+    get->next =last->next;
+    _ListSize--;
+    delete last;
+    return _head;
+  }
+
+  Node* TakeNode(int pos){
+    if(!_head){
+      cout<<"Is empty!"<<endl;
+      return NULL;
+    }
+    if(pos<0||pos>=_ListSize){
+      cout<<"0~ListSize-1!"<<"_ListSize ="<<_ListSize<<endl;
+      return NULL;
+    }
+    int count = pos;
+    Node* go = _head;
+    while(count--){
+      go = go->next;
+    }
+    return go;
+  }
+
+  Node* TakeHalf(Node*start,Node* end){
+    Node*fast = start;
+    Node*slow = start;
+    while(fast!=end){
+      fast = fast->next;
+      if(fast==end){return slow;}
+      slow = slow->next;
+      fast = fast->next;
+    }
+    if(slow==start){
+      cout<<"cha=0 or 1"<<endl;
+    }
+    return slow;
+  }
+
+  void CheckNode_ByAddress(Node*address){
+    cout<<"val:"<<address->val<<endl;
+    cout<<"Address:"<<address<<endl;
+    cout<<"next->Address:"<<address->next<<endl;
+  }
+
+  void CheckNode_Bypos(int pos){
+    if(pos<0||pos>=_ListSize){
+      cout<<"0~ListSzie!"<<_ListSize<<endl;
+      return;
+    }
+    Node* go = TakeNode(pos);
+    cout<<"val:"<<go->val<<endl;
+    cout<<"Address:"<<go<<endl;
+    cout<<"next->Address:"<<go->next<<endl;
+  }
+
+  void DisplayList(){
+    if(_head==NULL){
+      cout<<"Empty!"<<endl;
+      return;
+    }
+    Node* go = _head;
+    for(int i = 0;i<_ListSize;i++){
+      cout<<go<<"-"<<go->val<<"-"<<go->next<<endl;
+      go = go->next;
+    }
+  }
 };
--- SignleList/test.cpp
+++ SignleList/test.cpp
@@ -1,16 +1,119 @@
 #include"SList.h"
 
 int main(){
-  int arr[]={1,2,3,4,5,6,7,8,9,10};
+  int choose = 0;
+  char arr[]={'A','B','C','D','E','F'};
   int arrSize = sizeof(arr)/sizeof(arr[0]);
-  SList test(arr,arrSize);
-  ListNode* head = test.CreatAList_Loop(5);
-  //ListNode* newhead = test.SetNodeHead(11);
-  test.DisplayList();
-  //ListNode* end = test.TakeNode(10);
-  //ListNode* check = test.TakeHalf(newhead,end);
-  //cout<<check<<"-"<<check->val<<endl;
-  //test.DisplayList();
+  SList<char> test(arr,arrSize);
+  ListNode<char>* head = test.CreatAList();
+  cout<<"List Creat Succeed,Now begin test function!"<<endl;
+  cout<<"1.CheckNode_Bypos"<<" "<<"3.DelNodeHead"<<endl;
+  cout<<"4.DelNodeTail"<<" "<<"5,DelNodeInsert"<<endl;
+  cout<<"6.TakeHalf"<<" "<<"7,SetNodeAfter"<<" "<<"8.SetNodeBerfore"<<endl;
+  do{
+    cin>>choose;
+    switch(choose){
+      case 1:
+        {
+          cout<<"test CheckNode_Bypos!"<<endl;
+          int input1 = 0;
+          cin>>input1;
+          test.CheckNode_Bypos(input1);
+          test.DisplayList();
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 2:
+        {  cout<<"test CheckNode_ByAddress!"<<endl;
+         // ListNode<char>*address = NULL;
+         // cin>>address;
+         // test.CheckNode_ByAddress(address);
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 3:
+        {
+          cout<<"test DelNodeHead!"<<endl;
+          test.DelNodeHead();
+          test.DisplayList();
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 4:
+        {
+          cout<<"test DelNodeTail!"<<endl;
+          test.DelNodeTail();
+          test.DisplayList();
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 5:
+        {
+          cout<<"test DelNodeInsert!"<<endl;
+          int pos = 0;
+          cin>>pos;
+          test.DelNodeInsert(pos);
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 6:
+        {
+          cout<<"test TakeHalf!"<<endl;
+          ListNode<char>*start=test.TakeNode(0);
+          ListNode<char>*end=test.TakeNode(test.ListSize()-1);
+          cout<<test.TakeHalf(start,end)->val<<endl;
+          test.DisplayList();
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 7:
+        {
+          cout<<"test SetNodeAfter"<<endl;
+          int pos = 0;
+          char val = 0;
+          cin>>pos>>val;
+          test.DisplayList();
+          test.SetNodeAfter(pos,val);
+          test.DisplayList();
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 8:
+        {
+          cout<<"test SetNodeBerfore"<<endl;
+          int pos = 0;
+          char val = 0;
+          cin>>pos>>val;
+          test.DisplayList();
+          test.SetNodeBerfore(pos,val);
+          test.DisplayList();
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 9:
+        {
+          cout<<"test SetNodeHead"<<endl;
+          char val = 0;
+          cin>>val;
+          test.SetNodeHead(val);
+          test.DisplayList();
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      case 10:
+        {
+          cout<<"test SetNodeTail"<<endl;
+          char val = 0;
+          cin>>val;
+          test.SetNodeTail(val);
+          test.DisplayList();
+          cout<<"This function test finish!"<<endl;
+          break;
+        }
+      default:
+        cout<<"Type test fun number!"<<endl;
+    }
+  }while(choose!=0);
   return 0;
 
 }
