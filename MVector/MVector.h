e248332d3ad098b68c428e4c75f7cba83b45ff1c
coolsuperman <846069147@qq.com>
2019-04-14 19:22:13 +0800

    Basic Vector

--- MVector/MVector.h
+++ MVector/MVector.h
@@ -1,7 +1,8 @@
 
-#pragma once
+//#pragma once
 #include<iostream>
 #include<cassert>
+#include<cstring>
 using namespace std;
 
 namespace MV{
@@ -9,10 +10,17 @@ namespace MV{
     class MVector{
       public:
         typedef T* iterator;
+        typedef const T* const_iterator;
         iterator begin(){
           return _start;
         }
         iterator end(){
+          return _finish;
+        }
+        const_iterator cbegin()const{
+          return _start;
+        }
+        const_iterator cend()const{
           return _finish;
         }
         size_t Size(){
@@ -50,12 +58,67 @@ namespace MV{
           swap(_finish,v._finish);
           swap(_end_of_storage,v._end_of_storage);
         }
-        void Reserve(size_t n);
-        void Resize(size_t n,const T& val = T());
-        void PushBack(const T& x);
-        void PopBack();
-        void Insert(iterator pos,const T& x);
-        iterator Erase(iterator pos);
+        void Reserve(size_t n){
+          size_t size = Size();
+          if(n>Capacity()){
+            T* tmp = new T[n];
+            if(_start){
+              memcpy(tmp,_start,sizeof(T)*size);
+              delete[] _start;
+            }
+            _start = tmp;
+            _finish = _start+size;
+            _end_of_storage = _start+n;
+          }
+        }
+        void Resize(size_t n,const T& val = T()){
+          if(n<Size()){
+            _finish = _start+n;
+          }else{
+            if(n>Capacity()){
+              Reserve(n);
+            }
+            iterator begin = _finish;
+            while(begin<_start+n){
+              *begin = val;
+              ++begin;
+            }
+            _finish = _start+n;
+          }
+        }
+        void PushBack(const T& x){
+          Insert(_finish,x);
+        }
+        void PopBack(){
+          Erase(_finish);
+        }
+        void Insert(iterator pos,const T& x){
+          assert(pos<=_finish);
+          size_t offset = pos-_start;
+          if(_finish == _end_of_storage){
+            size_t newcapacity = Capacity()==0?1:Capacity()*2;
+            Reserve(newcapacity);
+            pos = _start+offset;//Die Dai Qi Shi Xiao
+          }
+          iterator end = _finish;
+          while(end>pos){
+            *end = *(end-1);
+            --end;
+          }
+          *pos = x;
+          ++_finish;
+          //pos++;
+        }
+        iterator Erase(iterator pos){
+          assert(pos<_finish);
+          iterator begin = pos;
+          while(begin<_finish-1){
+            *begin = *(begin+1);
+            ++begin;
+          }
+          --_finish;
+          return pos;
+        }
       private:
         T* _start;
         T* _finish;
--- MVector/test.cpp
+++ MVector/test.cpp
@@ -3,12 +3,18 @@
 void TestVector1(){
   MV::MVector<int> v1;
   v1.PushBack(1);
+  cout<<v1.Size()<<endl;
   v1.PushBack(2);
+  cout<<v1.Size()<<endl;
   v1.PushBack(3);
+  cout<<v1.Size()<<endl;
   v1.PushBack(4);
+  cout<<v1.Size()<<endl;
   for (size_t i = 0;i<v1.Size();i++){
-    cout<<v1[i]<<endl;
+    cout<<v1[i]  ;
   }
+  cout<<endl;
+  cout<<v1.Size()<<endl;
 }
 
 int main(){
